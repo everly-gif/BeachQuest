6ea295ad1c73ca3c9981bae856c4339cf65b3f56
everly-gif <everlyprecia@gmail.com>
2024-06-06 12:11:23 +0530

    loader

--- script.js
+++ script.js
@@ -31,14 +31,11 @@ window.onload = () => {
   document.body.appendChild(loaderDiv);
   
   videoBanner.onloadstart = function(){
-    loadingTimeout = setTimeout(() => {
-      document.getElementsByClassName('video-overlay')[0].style.display = 'none';
-      document.getElementsByClassName('loader-div')[0].style.display = 'flex';
-  }, 500); // 2000 milliseconds = 2 seconds
+    document.getElementsByClassName('video-overlay')[0].style.display = 'none';
+    document.getElementsByClassName('loader-div')[0].style.display = 'flex'
   }
 
   videoBanner.oncanplay = function () {
-    clearTimeout(loadingTimeout); // Clear the timeout if the video can play before 2 seconds
     document.getElementsByClassName('video-overlay')[0].style.display = 'block';
     document.getElementsByClassName('loader-div')[0].style.display = 'none';
  
